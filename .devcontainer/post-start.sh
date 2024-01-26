013acfb719de45b27c398f6db9b4a7b2340b6edf
Andrii Tsok <andrii@tsok.xyz>
2024-01-26 23:11:29 +0000

    chore: dev container scripts

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -5,13 +5,9 @@
 echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
-# Start postgres
-pg_ctl start
-
-
 # NATS
 export NATS_VERSION="2.10.9" && \
-cd tmp && \
+mkdir -p tmp && cd tmp && \
 wget https://github.com/nats-io/nats-server/releases/download/v$NATS_VERSION/nats-server-v$NATS_VERSION-linux-amd64.tar.gz && \
 tar -xzf nats-server-v$NATS_VERSION-linux-amd64.tar.gz && \
 sudo mv nats-server-v$NATS_VERSION-linux-amd64/nats-server /usr/local/bin/ 
@@ -24,6 +20,10 @@ yarn
 
 # Download Wundergraph Router
 npx wgc router download-binary -o ./tmp/wondergraph
+
+# Start postgres
+pg_ctl start
+
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
