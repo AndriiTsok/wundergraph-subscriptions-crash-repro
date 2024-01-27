edf3aff0827fb78aa37dda1da8aca751161a957f
Andrii Tsok <andrii@tsok.xyz>
2024-01-27 19:04:14 +0000

    chore: refactor

--- apps/subgraph-a/src/main.ts
+++ apps/subgraph-a/src/main.ts
@@ -5,6 +5,7 @@ import context from './app/context';
 const server = new ApolloServer({ schema, context });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT).then(({ url }) => {
+const HOSTNAME = process.env.HOSTNAME || '0.0.0.0';
+server.listen(PORT, HOSTNAME).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
