12a45e694962f5a9e508b50336bd343854f0bde4
Andrii Tsok <andrii@tsok.xyz>
2024-01-27 17:49:25 +0000

    chore: auto start nats

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -12,6 +12,7 @@ wget https://github.com/nats-io/nats-server/releases/download/v$NATS_VERSION/nat
 tar -xzf nats-server-v$NATS_VERSION-linux-amd64.tar.gz && \
 sudo mv nats-server-v$NATS_VERSION-linux-amd64/nats-server /usr/local/bin/ 
 
+nats-server&
 
 curl -sf https://binaries.nats.dev/nats-io/natscli/nats@latest | sh && sudo mv nats /usr/local/bin/
 
@@ -22,9 +23,7 @@ npm i -g nx
 
 # Download Wundergraph Router
 rm -rf ./tmp/wondergraph
-npx wgc router download-binary -o ./tmp/wondergraph
-
-nats-server&
+yarn wgc router download-binary -o ./tmp/wondergraph
 
 echo "post-start complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-start complete" >> "$HOME/status"
